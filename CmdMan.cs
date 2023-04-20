9d1432d01e7c038e9a4a6e82c697cb5044e4bed5
DevBadger10 <104037142+DevBadger10@users.noreply.github.com>
2023-04-20 11:52:29 +1000

    Initial Commit

---
+++ CmdMan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoronOS
+{
+    public class ComCall
+    {
+        public static void command(string str)
+        {
+            Console.WriteLine("Command called.");
+        }
+    }
+}
---
+++ Kernel.cs
@@ -0,0 +1,30 @@
+﻿using Cosmos.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace CosmosKernel1
+{
+    public class Kernel : Sys.Kernel
+    {
+        string cStart = "User@bOS]- ";
+        protected override void BeforeRun()
+        {
+            Console.Clear();
+            Console.WriteLine("BoronOS v" + BoronOS.Global.BOSVersion);
+        }
+
+        protected override void Run()
+        {
+            Console.Write(cStart);
+            var input = Console.ReadLine();
+            BoronOS.ComCall.command(input);
+        }
+
+        protected override void AfterRun()
+        {
+            Console.WriteLine("Exiting...");
+        }
+    }
+}
